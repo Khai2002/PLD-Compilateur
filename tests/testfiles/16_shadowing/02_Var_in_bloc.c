ed974b4170aab791a515be158f9ca6b9487dfc77
mohamedali05 <dalilajnefou@gmail.com>
2024-04-01 19:37:25 +0200

    added some test_cases

---
+++ tests/testfiles/16_shadowing/02_Var_in_bloc.c
@@ -0,0 +1,13 @@
+int x()
+{
+    return 3;
+}
+
+int main()
+{
+    {
+        int d;
+        d = 4;
+    }
+    return d;
+}
---
+++ tests/testfiles/16_shadowing/03_if.c
@@ -0,0 +1,13 @@
+int main()
+{
+
+    int a;
+    a = 1;
+    int x;
+    if (a == 1)
+    {
+        int a = 4;
+        a = 3;
+    }
+    return a;
+}
